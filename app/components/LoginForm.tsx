372e92cf2c82f5ec1ac729b20c17189ed20e78bf
ivanoou <pinjuh.ivano123@gmail.com>
2024-02-18 22:21:48 +0100

    started work on the home page

---
+++ app/components/LoginForm.tsx
@@ -0,0 +1,47 @@
+import { signup } from "./actions";
+
+export default function LoginForm() {
+
+  
+
+  return (
+    <div className="flex flex-grow flex-col items-center py-4 dark:bg-bg-d-300 bg-bg-l-200">
+      <h2 className="font-semibold text-3xl mb-8">
+        Sign Up For Free
+      </h2>
+
+      <form className="flex flex-col w-full gap-3 px-4">
+        <input className="dark:bg-bg-d-200 px-3 h-8 bg-bg-l-200 outline-none transition-all"
+          type="text"
+          autoComplete="off"
+          name="username" 
+          id="username" 
+          placeholder="Username" />
+
+        <input className="dark:bg-bg-d-200 px-3 h-8 bg-bg-l-200 outline-none transition-all"
+          type="email" 
+          name="email" 
+          id="email" 
+          placeholder="Email" />
+
+        <input className="dark:bg-bg-d-200 px-3 h-8 bg-bg-l-200 outline-none transition-all"
+          type="password" 
+          name="password" 
+          autoComplete="new-password"
+          id="password" 
+          placeholder="Password" />
+
+        <input className="dark:bg-bg-d-200 px-3 h-8 bg-bg-l-200 outline-none transition-all"
+          type="password" 
+          name="password2" 
+          id="password2" 
+          placeholder="Confirm Password" />
+
+          <button className="h-8 mt-6 bg-prim-200" formAction={signup}>
+            Sign Up
+          </button>
+      </form>
+
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
+import LoginForm from "./components/LoginForm";
+
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <main className="flex dark:bg-bg-d-100 bg-bg-l-100 min-h-screen flex-col items-center">
       helo this is home page
-      <Link className="text-white" href={"/login"}>
-        go to login
-      </Link>
 
       <Link className="text-white" href={"/private"}>
-        go to private
+        go to private page
       </Link>
       
+      <div className="w-full flex mt-20 gap-20 px-[15vw]">
+        <div className="bg-yellow-300 w-[50%]">img placeholder</div>
+        <LoginForm />
+      </div>
+
     </main>
   );
 }
