5a47893913d848abd49d16388cb4d532d8f98636
ivanoou <pinjuh.ivano123@gmail.com>
2024-02-20 22:20:11 +0100

    added form validation on home page

--- app/components/LoginForm.tsx
+++ app/components/LoginForm.tsx
@@ -1,9 +1,20 @@
 'use client'
 
 import { createBrowserClient } from "@supabase/ssr";
-import { login, signup } from "./actions";
+import { signup } from "./actions";
+import { useState, useEffect } from "react";
+import { FaLessThanEqual } from "react-icons/fa";
 
 export default function LoginForm() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [password2, setPassword2] = useState("")
+
+  const [isValid, setIsValid] = useState({form: false,
+                                          email: false,
+                                          password: false,
+                                          password2: true})
+
   const supabase = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
@@ -17,6 +28,54 @@ export default function LoginForm() {
       }
     })
   }
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      validateForm();
+    }, 300);
+
+    return () => clearInterval(intervalId);
+  }, [email, password, password2]);
+
+
+  const handleEmail = (e: any) => {
+    setEmail(e.target.value)
+  }
+  const handlePassword = (e: any) => {
+    setPassword(e.target.value)
+  }
+  const handlePassword2 = (e: any) => {
+    setPassword2(e.target.value)
+  }
+
+  const validateForm = () => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    let emailValid = false
+    let passValid = false
+    let pass2Valid = false
+    let formValid = false
+
+    if (password.length > 7) {
+      passValid = true
+    }
+
+    if (password === password2){
+      pass2Valid = true
+    }
+
+    if (email.match(emailRegex)) {
+      emailValid = true
+    }
+
+    if (emailValid && passValid && pass2Valid){
+      formValid = true
+    }
+
+    setIsValid({form: formValid,
+      email: emailValid,
+      password: passValid,
+      password2: pass2Valid})
+  }
   
 
   return (
@@ -25,47 +84,63 @@ export default function LoginForm() {
         Sign Up For Free
       </h2>
 
-      <form className="flex flex-col w-full gap-3 px-4">
-        <input className="dark:bg-bg-d-200 px-3 h-8 bg-bg-l-200 outline-none transition-all"
+      <form className="flex flex-col w-full gap-2 px-4">
+        <input className="dark:bg-bg-d-200 px-3 h-8 bg-bg-l-200 outline-none transition-all text-sm"
           type="text"
           autoComplete="off"
           name="username" 
           id="username" 
           placeholder="Username" />
 
-        <input className="dark:bg-bg-d-200 px-3 h-8 bg-bg-l-200 outline-none transition-all"
+
+        <input className={`${(!isValid.email && email.length > 0) && "border border-red-500"} dark:bg-bg-d-200 px-3 h-8 bg-bg-l-200 outline-none transition-all text-sm`}
+          onChange={handleEmail}
           type="email" 
           name="email" 
           id="email" 
-          placeholder="Email" />
+          placeholder="Email" 
+          required/>
 
-        <input className="dark:bg-bg-d-200 px-3 h-8 bg-bg-l-200 outline-none transition-all"
+
+        <input className={`${!(isValid.password || password.length < 1) && "border border-red-500"} dark:bg-bg-d-200 px-3 h-8 bg-bg-l-200 outline-none transition-all text-sm`}
+          onChange={handlePassword}
           type="password" 
           name="password" 
           autoComplete="new-password"
           id="password" 
-          placeholder="Password" />
+          placeholder="Password" 
+          required/>
 
-        <input className="dark:bg-bg-d-200 px-3 h-8 bg-bg-l-200 outline-none transition-all"
+        {!(isValid.password || password.length < 1) && <label htmlFor="password" className="text-xs mb-2">
+          Password must be 8 characters or more
+        </label>} 
+
+        <input className={`${(!isValid.password2 && password2.length > 3) && "border border-red-500"} dark:bg-bg-d-200 px-3 h-8 bg-bg-l-200 outline-none transition-all text-sm`}
+          onChange={handlePassword2}
           type="password" 
           name="password2" 
           id="password2" 
-          placeholder="Confirm Password" />
+          placeholder="Confirm Password" 
+          required/>
 
-          <button className="h-8 mt-6 bg-prim-100" formAction={signup}>
-            Sign Up
-          </button>
+        {(!isValid.password2 && password2.length > 3) && <label htmlFor="password" className="text-xs mb-2">
+          Password confirmation doesn't match password.
+        </label>}
+        
 
-          <button className="h-8 mt-1 bg-prim-100" formAction={login}>
-            Log In
-          </button>
-
-          <hr />
-
-          <button onClick={signInGoogle} className="h-8 bg-prim-200">
-            Sign Up with Google
-          </button>
+        <button disabled={!isValid.form} className={`${!isValid.form && "opacity-45"} h-8 mt-6 bg-prim-100`} formAction={signup}>
+          Sign Up
+        </button>
+        <hr />
       </form>
+
+      <div className="w-full px-4 mt-3">
+        <button onClick={signInGoogle} className="w-full h-8 bg-prim-100">
+          Sign Up with Google
+        </button>
+      </div>
+
+      
 
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,81 @@
 import Image from "next/image";
 import Link from "next/link";
 import LoginForm from "./components/LoginForm"
-import { redirect } from "next/navigation";
-
-
 
 
 export default function Home() {
   
   
   return (
-    <main className="flex dark:bg-bg-d-100 bg-bg-l-100 min-h-screen flex-col items-center">
-      helo this is home page
-
+    <main className="flex dark:bg-bg-d-100 bg-bg-l-100 min-h-screen flex-col items-center px-[15vw]">
       <Link className="text-white" href={"/private"}>
         go to private page
       </Link>
       
-      <div className="w-full flex mt-20 gap-20 px-[15vw]">
-        <div className="bg-yellow-300 w-[50%]">img placeholder</div>
+      <div className="w-full flex mt-32 gap-20">
+        <div className="bg-yellow-300 w-[45%]">img placeholder</div>
         <LoginForm />
       </div>
+
+      <div className="w-full mt-32">
+        <h2 className="w-full text-center text-5xl font-semibold">
+          Track Your Habits
+        </h2>
+
+        <div className="w-full flex justify-between mt-16">
+          <div className="flex flex-col w-[29%] border">
+            <h4 className="text-3xl font-semibold">
+              First Card
+            </h4>
+
+            <p className="text-sm font-light mt-4 mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Nostrum nam sequi ut tenetur facere vero quidem atque obcaecati fugit sed.
+            </p>
+
+            <div className="w-full bg-prim-200 h-40">
+              img placeholder
+            </div>
+          </div>
+
+          <div className="flex flex-col w-[29%] border">
+            <h4 className="text-3xl font-semibold">
+              Second Card
+            </h4>
+
+            <p className="text-sm font-light mt-4 mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Nostrum nam sequi ut tenetur facere vero quidem atque obcaecati fugit sed.
+            </p>
+
+            <div className="w-full bg-prim-200 h-40">
+              img placeholder
+            </div>
+          </div>
+
+          <div className="flex flex-col w-[29%] border">
+            <h4 className="text-3xl font-semibold">
+              Third Card
+            </h4>
+
+            <p className="text-sm font-light mt-4 mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Nostrum nam sequi ut tenetur facere vero quidem atque obcaecati fugit sed.
+            </p>
+
+            <div className="w-full bg-prim-200 h-40">
+              img placeholder
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+
+      <div className="w-full mt-20">
+
+      </div>
+
     </main>
   );
 }
