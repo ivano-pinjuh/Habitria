b1494c7aaa2670347c205c22c964963408cb8ec3
ivanoou <pinjuh.ivano123@gmail.com>
2024-02-29 22:21:45 +0100

    started working on the /notes page

--- app/(signedIn)/notes/page.tsx
+++ app/(signedIn)/notes/page.tsx
@@ -19,28 +19,20 @@ export default async function NotesPage() {
   }
 
   return (
-    <div className="w-full">
+    <div className="w-full flex">
       <div className='dark:pattern-hive-purple-500/10 pattern-hive-purple-500/15 pattern-hive-scale-75 opacity-80 fixed top-0 left-0 h-screen w-full -z-50'></div>
       
-      <div className='w-full bg-bg-l-300 dark:bg-bg-d-200 h-44 shadow-lg'>
+      <div className='lg:w-[16%] h-[calc(100svh-64px)] bg-bg-l-300 dark:bg-bg-d-200'>
       </div>
 
-      <div className='w-full flex md:flex-row flex-col gap-y-20 md:justify-between px-10 mt-10 '>
-        
-      </div>
-
-      <div className='w-full'>
+      <div className='flex-grow ml-10'>
         {!(error || !data?.user) && <form action={signOut}><button className="bg-prim-100 w-40 mt-6">sign out</button></form>}
 
         <p>Hello {data.user.email}</p>
         <p>Hello {data.user.user_metadata.name}</p>
       </div>
-      
 
-      <div>
-        {!(error || !data?.user) && <form action={signOut}><button className="bg-prim-100 w-40 mt-6">sign out</button></form>}
-        <p>Hello {data.user.email}</p>
-      </div>
+      
     </div>
   )
 }
