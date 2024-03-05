29d34f4716cc531b2170d414dfffb069d9205041
ivanoou <pinjuh.ivano123@gmail.com>
2024-03-05 20:18:32 +0100

    not much progress

--- app/(signedIn)/notes/page.tsx
+++ app/(signedIn)/notes/page.tsx
@@ -18,14 +18,22 @@ export default async function NotesPage() {
       <div className='w-full bg-bg-l-300 dark:bg-bg-d-200 h-44 px-10 py-8 shadow-lg'>
       </div>
 
-      <div className='px-10 mt-6 flex w-full lg:w-[40%] m-auto'>
-        <input className='placeholder:font-semibold w-full duration-300 bg-bg-l-100 dark:bg-bg-d-300 h-12 px-4 rounded outline-none shadow-md transition-all' 
+      <div className='px-10 mt-6 flex flex-col gap-2 w-full lg:w-[40%] m-auto group'>
+        <input className='placeholder:font-semibold w-full flex-wrap duration-300 bg-bg-l-100 dark:bg-bg-d-300 h-12 min-h-fit px-4 rounded outline-none shadow-md transition-all' 
           type="text" 
           placeholder='Add a Note'
           name="add-note" 
           id="add-note"
           autoComplete="off" />
+
+        <textarea className='w-full break-all h-auto duration-300 bg-bg-l-100 dark:bg-bg-d-300 min-h-12 px-4 pt-2 rounded outline-none shadow-md transition-all'
+          placeholder='Note text...'
+          name="note-desc" 
+          id="note-desc"
+        >
           
+        </textarea>
+
       </div>
 
       <div className='px-32 mt-10 flex justify-between flex-wrap'>
